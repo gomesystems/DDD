5ce4bd9a5c932e0bbe44701d74590af470884fd0
Carlos <carlos.gomesystems@gmail.com>
2022-04-26 23:12:05 -0300

    Add project files.

---
+++ DDDWebAPI.Domain.Core/Interfaces/Services/IServiceProduto.cs
@@ -0,0 +1,9 @@
+﻿using DDDWebAPI.Domain.Models;
+
+namespace DDDWebAPI.Domain.Core.Interfaces.Services
+{
+    public interface IServiceProduto : IServiceBase<Produto>
+    {
+        void Dispose();
+    }
+}
